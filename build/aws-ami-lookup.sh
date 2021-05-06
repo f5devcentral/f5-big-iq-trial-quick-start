e2f905d3589b6b3c2fa7278b74c77662d80cd3ff
Roman Jouhannet <r.jouhannet@f5.com>
2021-05-06 08:15:20 -0700

    update with 8.0.0.1

--- build/aws-ami-lookup.sh
+++ build/aws-ami-lookup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #set -x
 
-echo "Searching for image where name begins with $1"
-# e.g. "F5 Networks BYOL BIG-IQ-7.1.0.3.0.0.41, F5 Hourly BIG-IQ-8.0.0-0.0.594"
+echo "Searching for image where name contains $1"
+# e.g. "BIG-IQ-7.1.0.3, BIG-IQ-8.0.0-0.0.594, BIG-IQ-8.0.0.1-0.0.41"
 regions=$(aws ec2 describe-regions --output text --query 'Regions[*].RegionName')
 json=""
 for region in $regions; do
-    ami=$(aws ec2 describe-images --region $region --filters Name=is-public,Values=true Name=name,Values="$1*" --output json | jq '.Images[] | "\(.ImageId)"' | head -1)
+    ami=$(aws ec2 describe-images --region $region --filters Name=is-public,Values=true Name=name,Values="*$1*" --output json | jq '.Images[] | "\(.ImageId)"' | head -1)
     if [ ! -z "$ami" ]; then
         json="$json,\"$region\": { \"bigiq\": $ami}"
     else
